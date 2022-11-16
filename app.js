5090bd4753540614e10a364d7cde2c0a5735eb19
efecollins <efejamb@gmail.com>
2022-11-16 09:46:31 +0100

    using passport to add cookies & sessions

--- app.js
+++ app.js
@@ -41,7 +41,28 @@ app.get('/login', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
+    const user = new Users({
+        username: req.body.username,
+        password: req.body.password
+    })
 
+    req.login(user, err => {
+        if (err) {
+            console.log(err)
+        } else {
+            passport.authenticate('local')(req, res, () => {
+                res.redirect('/secrets')
+            })
+        }
+    })
+})
+
+app.get('/secrets', (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render('secrets')
+    } else {
+        res.redirect('/login')
+    }
 })
 
 app.get('/register', (req, res) => {
@@ -50,12 +71,21 @@ app.get('/register', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-    User.register({username: req.body.username}, req.body.password, (err, user) => {
+    Users.register({username: req.body.username}, req.body.password, (err, user) => {
         if(err) {
             console.log(err)
             res.redirect('/register')
+        } else {
+            passport.authenticate('local')(req, res, () => {
+                res.redirect('/secrets')
+            })
         }
     })
+})
+
+app.get('/logout', (req, res) => {
+    req.logout(err => {if(err){console.log(err)}})
+    res.redirect('/')
 })
 
 app.listen(3000, () => {

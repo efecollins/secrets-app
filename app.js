5ee0a7e0756b382c9c11d96177718bfc480c90dc
efecollins <efejamb@gmail.com>
2022-11-22 01:06:09 +0100

    Allow multiple callbackURL for google oauth

--- app.js
+++ app.js
@@ -48,7 +48,8 @@ passport.deserializeUser(function (user, cb) {
 
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: 'http://localhost:3001/auth/google/secrets'||'https://secret.cyclic.app/auth/google/secrets'||'https://lemon-dugong-gown.cyclic.app/auth/google/secrets'
 },
     function (accessToken, refreshToken, profile, cb) {
         Users.findOrCreate({ googleId: profile.id }, function (err, user) {
